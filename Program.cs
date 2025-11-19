ca1f915cdb145c1b15fc6d523f4b39177dd22c67
arnold-tj <tj.arnold.torrez.z@upds.net.bo>
2025-11-19 09:38:24 -0400

    Change server URL to listen on port 8080

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.WebHost.UseUrls("http://0.0.0.0:8080");
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -23,3 +24,4 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
